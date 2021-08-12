4b1e8990a19bd89c9fe6164337dc18cb61c84c24
QuangHuyNguyen1605 <quang.huy165@gmail.com>
2021-08-12 20:58:16 +0200

    modified dataset and model

---
+++ src/app/members.ts
@@ -0,0 +1,87 @@
+export interface Member {
+  id: number;
+  firstName: string;
+  lastName: string;
+  age: number;
+  gender: string;
+}
+
+export const members = [
+  {
+    id: 1,
+    firstName: 'Dieu Ly',
+    lastName: 'Ninh',
+    age: 26,
+    gender: 'female'
+  },
+  {
+    id: 2,
+    firstName: 'Quang Huy',
+    lastName: 'Nguyen',
+    age: 27,
+    gender: 'male'
+  },
+  {
+    id: 3,
+    firstName: 'Tuan Nghia',
+    lastName: 'Luu',
+    age: 26,
+    gender: 'male'
+  },
+  {
+    id: 4,
+    firstName: 'Huu Giang',
+    lastName: 'Nguyen',
+    age: 27,
+    gender: 'male'
+  },
+  {
+    id: 5,
+    firstName: 'Thi Dieu Linh',
+    lastName: 'Vo',
+    age: 26,
+    gender: 'female'
+  },
+  {
+    id: 6,
+    firstName: 'Manh Cuong',
+    lastName: 'Mai',
+    age: 26,
+    gender: 'male'
+  },
+  {
+    id: 7,
+    firstName: 'Hoang Hung',
+    lastName: 'Do',
+    age: 29,
+    gender: 'male'
+  },
+  {
+    id: 8,
+    firstName: 'Huong Thu',
+    lastName: 'Nguyen',
+    age: 25,
+    gender: 'female'
+  },
+  {
+    id: 9,
+    firstName: 'Hong Anh',
+    lastName: 'Duong',
+    age: 26,
+    gender: 'female'
+  },
+  {
+    id: 10,
+    firstName: 'Quang Tuan',
+    lastName: 'Nguyen',
+    age: 26,
+    gender: 'male'
+  },
+  {
+    id: 11,
+    firstName: 'Nguoi yeu',
+    lastName: 'Tuan',
+    age: 24,
+    gender: 'female'
+  }
+];
